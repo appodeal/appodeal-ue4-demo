63efd8c01abdb8e1758d3a485151d29dd2597e7d
Nazar Kozak <nzrkzk@gmail.com>
2016-09-14 11:42:36 +0300

    init

---
+++ AppodealUE4Demo/Source/AppodealUE4Demo/AppodealUE4Demo.cpp
@@ -0,0 +1,5 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "AppodealUE4Demo.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, AppodealUE4Demo, "AppodealUE4Demo" );
