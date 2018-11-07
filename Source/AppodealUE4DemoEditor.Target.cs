9db108fbefda1ca584aec844ca7ed732a2e37f7b
Nazar Kozak <nzrkzk@gmail.com>
2018-11-07 14:59:38 +0200

    full demo rework

--- Source/AppodealUE4DemoEditor.Target.cs
+++ Source/AppodealUE4DemoEditor.Target.cs
@@ -3,23 +3,24 @@
 using UnrealBuildTool;
 using System.Collections.Generic;
 
-public class AppodealUE4DemoTarget : TargetRules
+public class AppodealUE4DemoEditorTarget : TargetRules
 {
-	public AppodealUE4DemoTarget(TargetInfo Target)
+	public AppodealUE4DemoEditorTarget(TargetInfo Target): base(Target)
 	{
-		Type = TargetType.Game;
+		Type = TargetType.Editor;
+		ExtraModuleNames.Add("AppodealUE4Demo");
 	}
 
 	//
 	// TargetRules interface.
 	//
 
-	public override void SetupBinaries(
+	/*public override void SetupBinaries(
 		TargetInfo Target,
 		ref List<UEBuildBinaryConfiguration> OutBuildBinaryConfigurations,
 		ref List<string> OutExtraModuleNames
 		)
 	{
 		OutExtraModuleNames.AddRange( new string[] { "AppodealUE4Demo" } );
-	}
+	}*/
 }
